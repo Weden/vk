0fd4cd0186955c8b9d12380755b92c1fa241a80b
err0x0 <tz.4678@gmail.com>
2014-06-17 12:59:21 +0400

    Добавил выбор клиента

--- vkapp.py
+++ vkapp.py
@@ -6,16 +6,31 @@ class VKApp(VKClient):
     def __init__(self, *args, **kwargs):
         super(VKApp, self).__init__(*args, **kwargs)
         self.process_captcha = process_captcha
-        # если приложение установлено у более чем 1 миллиона человек, то можно отправлять не более 35 запросов в секунду
-        self.delay_time = 1.0 / 35
+        self.delay_time = 1.0 / 5
+        self.set_client('android')
+
+    def set_client(self, client=None):
+        client = str(client).lower()
+        # офиц приложение под android
+        if client == 'android':
+            self.client_id = 2274003
+            self.client_secret = 'hHbZxrka2uZ6jB1inYsH'
+            self.scope = 'notify,friends,photos,audio,video,docs,notes,wall,groups,messages,notifications,activity,status,pages,stats'
+            self.grant_type = 'password'
+            self.auth_type = 'token'
+        elif client == 'win8': # blocked
+            self.client_id = 2692017
+            self.client_secret = 'rqIkXYBauPjqE2EVxB2j'
+            self.scope = 'notify,friends,photos,audio,video,docs,notes,wall,groups,messages,notifications,activity,status,pages,stats'
+            self.grant_type = 'password'
+            self.auth_type = 'token'
 
     def login(self, username, password):
         self.auth({
-                       'username': username,
-                       'password': password,
-                       'client_id': 2274003,
-                       'client_secret': 'hHbZxrka2uZ6jB1inYsH',
-                       'scope': 'notify,friends,photos,audio,video,docs,notes,wall,groups,messages,notifications,activity,status,pages,stats',
-                       'grant_type': 'password'
-                  }, 
-                  'token')
+            'username': username,
+            'password': password,
+            'client_id': self.client_id,
+            'client_secret': self.client_secret,
+            'scope': self.scope,
+            'grant_type': self.grant_type
+        }, self.auth_type)
